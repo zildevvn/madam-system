9ce06d3f95dea5820cabc3dcad58f966f02e1c1c
zildevvn <vuongdevvn0509@gmail.com>
2026-03-19 17:00:26 +0700

    Add feature order

--- resources/js/components/Layout.jsx
+++ resources/js/components/Layout.jsx
@@ -6,11 +6,8 @@ export default function Layout() {
     return (
         <div className="min-h-screen bg-gray-50">
             <Header />
-
-            <main className="py-10">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <Outlet />
-                </div>
+            <main>
+                <Outlet />
             </main>
         </div>
     );
