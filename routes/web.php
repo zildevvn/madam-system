89e481eb20a1f51883ce1caddc0af972eaf0561f
zildevvn <vuongdevvn0509@gmail.com>
2026-04-01 10:55:24 +0700

    add funtion test pusher

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test-pusher', function () {
+    event(new \App\Events\TestEvent('Hello from host!'));
+    return "Event Broadcasted!";
+});
+
 Route::get('/{any?}', function () {
+
     return view('welcome');
 })->where('any', '^(?!api).*$');
