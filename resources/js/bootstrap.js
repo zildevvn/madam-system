4d9cc1b94420288ff2d06b7672913bc7b4ca5cc2
zildevvn <vuongdevvn0509@gmail.com>
2026-04-01 09:58:22 +0700

    refactor Reverb to Pusher

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,14 +8,11 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
-const isHttps = import.meta.env.VITE_REVERB_SCHEME === 'https';
-
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
-    wsPort: import.meta.env.VITE_REVERB_PORT || 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT || 8080,
-    forceTLS: isHttps,
-    enabledTransports: isHttps ? ['ws', 'wss'] : ['ws'],
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+    enabledTransports: ['ws', 'wss'],
 });
+
