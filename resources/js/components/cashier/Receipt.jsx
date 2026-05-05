cad73a57bb08039d624ea63bb3c730fa0d5d0ee8
zildevvn <vuongdevvn0509@gmail.com>
2026-05-05 10:46:03 +0700

    update Receipt

--- resources/js/components/cashier/Receipt.jsx
+++ resources/js/components/cashier/Receipt.jsx
@@ -76,7 +76,7 @@ const Receipt = ({ order, tableName, allTables, discountType = 'fixed', discount
         return Number(a) - Number(b);
     });
 
-    const showTableHeaders = groupedItems.length > 1 || isGroupReservation;
+    const showTableHeaders = groupedItems.length > 1 || isGroupReservation || !!order.mergedTables;
 
     return (
         <div id="receipt-print-area" className="receipt-print-only">
@@ -196,7 +196,7 @@ const Receipt = ({ order, tableName, allTables, discountType = 'fixed', discount
                                     {showTableHeaders && (
                                         <tr className="receipt-subtotal-row" style={{ marginBottom: '8px' }}>
                                             <td colSpan="2" align="right" style={{ borderTop: '1px dashed #eee', padding: '6px 0', fontSize: '9px', fontStyle: 'italic', color: '#666' }}>
-                                                Cộng {isSharedSection ? 'phần chung' : `bàn ${tGroup}`}:
+                                                Cộng {isSharedSection ? 'phần chung' : `bàn ${resolveTableLabel(tGroup)}`}:
                                             </td>
                                             <td align="right" style={{ borderTop: '1px dashed #eee', padding: '6px 0', fontSize: '9px', fontWeight: 'bold' }}>
                                                 {formatPrice(sectionGross - sectionDiscount)}
